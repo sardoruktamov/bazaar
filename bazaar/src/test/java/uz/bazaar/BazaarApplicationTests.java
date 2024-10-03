3b5b204b30ca917df7f9333c69a43ce8d5027f6e
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-10-03 21:32:18 +0500

    create project

---
+++ bazaar/src/test/java/uz/bazaar/BazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.bazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
