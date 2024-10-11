4d656178ff95c616f62ac23c2026eb2141039fe6
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-10-11 22:36:16 +0500

    update BazaarService.java

--- bazaar/src/main/java/uz/bazaar/marketzone/controller/BazaarController.java
+++ bazaar/src/main/java/uz/bazaar/marketzone/controller/BazaarController.java
@@ -7,20 +7,30 @@ import uz.bazaar.marketzone.dto.BazaarDto;
 import uz.bazaar.marketzone.dto.ResponseDto;
 import uz.bazaar.marketzone.service.BazaarService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/bazaar")
 @RequiredArgsConstructor
 public class BazaarController {
 
     private final BazaarService bazaarService;
+
+    @PostMapping
+    public ResponseDto<BazaarDto> addBazaar(@Valid @RequestBody BazaarDto bazaarDto){
+        return bazaarService.addBazaar(bazaarDto);
+    }
+
     @GetMapping("/{id}")
     public ResponseDto<BazaarDto> getId(@PathVariable Integer id){
         return bazaarService.getById(id);
     }
 
-    @PostMapping
-    public ResponseDto<BazaarDto> addBazaar(@Valid @RequestBody BazaarDto bazaarDto){
-        return bazaarService.addBazaar(bazaarDto);
+    @GetMapping("/search")
+    public List<BazaarDto> searchBazaars(@RequestParam(required = false) String stir,
+                                         @RequestParam(required = false) String name,
+                                         @RequestParam(required = false) String phoneNumber) {
+        return bazaarService.findByParams(stir, name, phoneNumber);
     }
 
     @DeleteMapping("/{id}")
