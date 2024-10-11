3675c287b91853fc2272a3aa854910de5c6bdea2
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-10-11 22:35:51 +0500

    update BazaarRepository.java

--- bazaar/src/main/java/uz/bazaar/marketzone/repository/BazaarRepository.java
+++ bazaar/src/main/java/uz/bazaar/marketzone/repository/BazaarRepository.java
@@ -1,9 +1,22 @@
 package uz.bazaar.marketzone.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uz.bazaar.marketzone.model.Bazaar;
 
+import java.util.List;
+
 @Repository
 public interface BazaarRepository extends JpaRepository<Bazaar,Integer> {
+
+    @Query("SELECT b FROM Bazaar b WHERE " +
+            "(:stir IS NULL OR b.stir = :stir) AND " +
+            "(:name IS NULL OR b.name = :name) AND " +
+            "(:phoneNumber IS NULL OR b.phoneNumber = :phoneNumber)")
+    List<Bazaar> findByParams(@Param("stir") String stir,
+                              @Param("name") String name,
+                              @Param("phoneNumber") String phoneNumber);
+
 }
