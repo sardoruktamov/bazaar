4f711f7f14daf3137a9b6f3912c5d98977b06292
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-10-15 06:59:10 +0500

    create SubCategory Entity

---
+++ bazaar/src/main/java/uz/bazaar/marketzone/model/SubCategory.java
@@ -0,0 +1,31 @@
+package uz.bazaar.marketzone.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "sub_category")
+public class SubCategory {
+
+    @Id
+    @GeneratedValue(generator = "subcategory_seq")
+    @SequenceGenerator(name = "subcategory_seq", sequenceName = "subcategory_id_seq", allocationSize = 1)
+    private Integer id;
+
+    @Column(name = "category_id", nullable = false)
+    private Integer categoryId;
+
+    @NotNull(message = "name cannot be null")
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+
+}
