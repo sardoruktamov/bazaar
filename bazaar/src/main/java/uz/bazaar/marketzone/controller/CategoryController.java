4da6f52bbc3edddb25161b8e22be9fa1488caab3
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-10-15 07:16:54 +0500

    update CategoryController, add POST pethod

--- bazaar/src/main/java/uz/bazaar/marketzone/controller/CategoryController.java
+++ bazaar/src/main/java/uz/bazaar/marketzone/controller/CategoryController.java
@@ -1,11 +1,25 @@
 package uz.bazaar.marketzone.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uz.bazaar.marketzone.dto.BazaarDto;
+import uz.bazaar.marketzone.dto.CategoryDto;
+import uz.bazaar.marketzone.dto.ResponseDto;
+import uz.bazaar.marketzone.service.CategoryService;
 
 @RestController
 @RequestMapping("/categories")
 @RequiredArgsConstructor
 public class CategoryController {
+
+    public final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseDto<CategoryDto> addCategory(@Valid @RequestBody CategoryDto categoryDto){
+        return categoryService.addCategory(categoryDto);
+    }
 }
