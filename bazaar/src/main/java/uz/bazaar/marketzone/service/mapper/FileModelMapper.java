af72109711e60b593297c47e446c28832e5b8be9
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2024-10-24 17:56:24 +0500

    update FileModelMapper

--- bazaar/src/main/java/uz/bazaar/marketzone/service/mapper/FileModelMapper.java
+++ bazaar/src/main/java/uz/bazaar/marketzone/service/mapper/FileModelMapper.java
@@ -1,4 +1,12 @@
 package uz.bazaar.marketzone.service.mapper;
 
+import org.mapstruct.Mapper;
+import uz.bazaar.marketzone.dto.FileModelDTO;
+import uz.bazaar.marketzone.model.FileModel;
+
+@Mapper(componentModel = "spring")
 public interface FileModelMapper {
+    FileModelDTO toDTO(FileModel fileModel);
+
+    FileModel toEntity(FileModelDTO fileModelDTO);
 }
