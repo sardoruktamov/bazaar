63aa26c667f2638136a5636eff94251cb4e8da63
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-12-09 21:34:02 +0500

    create RegistrationDTO

---
+++ bazaar/src/main/java/uz/bazaar/marketzone/dto/RegistrationDTO.java
@@ -0,0 +1,30 @@
+package uz.bazaar.marketzone.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistrationDTO {
+
+    @NotBlank(message = "firstName is null or not contains any character")
+    private String firstName;
+    @NotBlank(message = "lastName is null or not contains any character")
+    private String lastName;
+    private String username;
+    @Size(min = 6, message = "Password must contain at least 6 character")
+    private String password;
+    private String phoneNumber;
+    @Email(message = "Email is incorrect")
+    private String email;
+}
