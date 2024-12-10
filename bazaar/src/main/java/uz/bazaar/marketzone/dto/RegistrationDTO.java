d8ac8c3bd3b2531d3ae648d30648883a516f4edf
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-12-10 20:16:34 +0500

    UPDATE FOR VALIDATION

--- bazaar/src/main/java/uz/bazaar/marketzone/dto/RegistrationDTO.java
+++ bazaar/src/main/java/uz/bazaar/marketzone/dto/RegistrationDTO.java
@@ -21,9 +21,11 @@ public class RegistrationDTO {
     private String firstName;
     @NotBlank(message = "lastName is null or not contains any character")
     private String lastName;
+    @NotBlank(message = "username is null or not contains any character")
     private String username;
     @Size(min = 6, message = "Password must contain at least 6 character")
     private String password;
+    @NotBlank(message = "phoneNumber is null or not contains any character")
     private String phoneNumber;
     @Email(message = "Email is incorrect")
     private String email;
